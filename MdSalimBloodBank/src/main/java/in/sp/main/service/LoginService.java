ab6efa6648e567a770eb65103dad4beca55ca8df
salim124421 <786rps786@gmail.com>
2024-03-11 11:52:24 +0530

    md salim

---
+++ MdSalimBloodBank/src/main/java/in/sp/main/service/LoginService.java
@@ -0,0 +1,10 @@
+package in.sp.main.service;
+
+import java.util.List;
+
+import in.sp.main.beans.User;
+
+public interface LoginService
+{
+	public boolean loginService(String email, String password);
+}
