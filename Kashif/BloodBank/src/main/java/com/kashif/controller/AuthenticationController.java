899f89f23feec8e381adc8c9d8bec240a5146a42
Kashif <kashifnezam123@gmail.com>
2024-03-13 16:12:59 +0530

    changed

--- Kashif/BloodBank/src/main/java/com/kashif/controller/AuthenticationController.java
+++ Kashif/BloodBank/src/main/java/com/kashif/controller/AuthenticationController.java
@@ -7,8 +7,7 @@ import com.kashif.repository.UserRepo;
 import com.kashif.service.UserRegistrationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +17,7 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class AuthenticationController {
-    private int loginAttempts = 0;
-    @Autowired
-    UserRepo userRepo;
+
     @Autowired
     private UserRegistrationService userRegistrationService;
 
@@ -33,21 +30,17 @@ public class AuthenticationController {
 
     // Users get Registered and Success Message
 
-    @PostMapping(value = "/registration")
+    @PostMapping("/registration")
     public String register(RegistrationDTO registrationDTO, Model mp) {
-        if (userRepo.existsByUsername(registrationDTO.getUsername())) {
+        if (userRegistrationService.existsByUsernamer(registrationDTO.getUsername())) {
             mp.addAttribute("errorMsg", "Username Already Exists");
             return "signup";
         }
-        try {
-            RegistrationDTO registeredUser = userRegistrationService.registerUser(registrationDTO);
 
+            RegistrationDTO registeredUser = userRegistrationService.registerUser(registrationDTO);
             mp.addAttribute("success", "Account Created Successfully");
             return "login";
-        } catch (Exception ex) {
-            mp.addAttribute("errorMsg", ex);
-            return "error";
-        }
+        
     }
 
 
@@ -61,43 +54,7 @@ public class AuthenticationController {
     //----- Got the Login details and redirected to Dashboard -----
     @PostMapping("/login")
     public String loginChk(LoginDTO loginDTO, HttpSession session, Model model) {
-
-        session.setAttribute("loginAttempts", loginAttempts);
-
-        String username = loginDTO.getUsername();
-        String password = loginDTO.getPassword();
-        if (username==null || password==null || username.isEmpty() || password.isEmpty()) {
-
-            model.addAttribute("errorMsg", "Login failed: Invalid Username ");
-            return "login";
-        }
-        if (!userRepo.existsByUsername(username)) {
-            model.addAttribute("errorMsg", "Login failed: Invalid Username ");
-            return "login";
-        }
-        if (userRepo.getBlockedStatusByUsername(username)) {
-            model.addAttribute("errorMsg", "User BLOCKED ");
-            return "login";
-        }
-
-        String pass = userRepo.getPasswordByUsername(username);
-        if (pass.equals(password)) {
-            session.setAttribute("data", userRepo.findByUsername(username).get());
-            return "redirect:/dashboard";
-        } else {
-            loginAttempts++;
-        }
-
-        if (loginAttempts > 3) {
-            userRepo.updateBlockedStatusByUsername(true, username);
-            session.invalidate(); // Invalidate the session to clear cache
-            model.addAttribute("errorMsg", "Login failed: User BLOCKED ");
-
-            return "login";
-        }
-        model.addAttribute("errorMsg", "Login failed: Invalid Password ");
-        model.addAttribute("times", loginAttempts);
-        return "login";
+        return userRegistrationService.loginCheck(loginDTO, session, model);
     }
 
 
@@ -110,44 +67,13 @@ public class AuthenticationController {
     // ----- Updating Password and changing the newUser to False -----
     @PostMapping("/update-password")
     public String validatePassword(@RequestParam String currentPassword, @RequestParam String newPassword, @RequestParam String confirmPassword, Model mp, HttpSession session) {
-        UserRegistration userData = (UserRegistration) session.getAttribute("data");
-        if (!newPassword.equals(confirmPassword)) {
-            mp.addAttribute("errorMsg", "Password didn't Match.");
-            return "update-password";
-        }
-        if (currentPassword.equals(newPassword)) {
-            mp.addAttribute("errorMsg", "Can't set old password");
-            return "update-password";
-        }
-        if (!currentPassword.equals(userData.getPassword())) {
-            mp.addAttribute("errorMsg", "Wrong Password");
-            return "update-password";
-        }
-
-        String username = userData.getUsername();
-        System.out.println(userData.getUsername());
-        //---------- Updated the Password ---------
-        userRepo.updatePasswordByUsername(newPassword, username);
-        userRepo.updateNewUserByUsername(false, username);
-        mp.addAttribute("success", "Password updated successfully.");
-        return "login";
+        return userRegistrationService.validateLogin(currentPassword, newPassword, confirmPassword, mp, session);
     }
 
     //----- After redirecting to Dashboard -----
     @RequestMapping("/dashboard")
     public String dashboard(Model mp, HttpSession session) {
-        UserRegistration userData = (UserRegistration) session.getAttribute("data");
-        mp.addAttribute("data", userData);
-        if (userData.isNewUser()) {
-            return "redirect:/update-password";
-        }
-        if (userData.getRole().equalsIgnoreCase("admin")) {
-
-            mp.addAttribute("allUsers", userRepo.findAllEndUsers());
-            return "admin-dashboard";
-        }
-        if (userData.getRole().equals("agent")) return "agent-dashboard";
-        return "enduser-dashboard";
+       return userRegistrationService.dashboard(mp, session);
     }
 
 
--- Kashif/BloodBank/src/main/java/com/kashif/service/UserRegistrationService.java
+++ Kashif/BloodBank/src/main/java/com/kashif/service/UserRegistrationService.java
@@ -1,61 +1,66 @@
 package com.kashif.service;
+
+import com.kashif.dto.LoginDTO;
 import com.kashif.dto.RegistrationDTO;
 import com.kashif.entity.UserRegistration;
 import com.kashif.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.util.*;
 
 @Service
 public class UserRegistrationService {
+    private int loginAttempts = 0;
+    @Autowired
+    private UserRegistration userRegistration;
 
     @Autowired
-    private UserRegistration userRegistration ;
-
-    @Autowired
-    private UserRepo repo;
+    private UserRepo userRepo;
 
 
-    public RegistrationDTO registerUser(RegistrationDTO registrationDTO){
+    public RegistrationDTO registerUser(RegistrationDTO registrationDTO) {
 
-        if(registrationDTO.getUsername()!=null && !registrationDTO.getUsername().isEmpty() && !repo.existsByUsername(registrationDTO.getUsername())){
+        if (registrationDTO.getUsername() != null && !registrationDTO.getUsername().isEmpty() && !userRepo.existsByUsername(registrationDTO.getUsername())) {
             userRegistration.setUsername(registrationDTO.getUsername());
-        }else {
+        } else {
             throw new RuntimeException("Username might be  null or Already Exists");
         }
-        if(registrationDTO.getName()!=null && !registrationDTO.getName().isEmpty()){
+        if (registrationDTO.getName() != null && !registrationDTO.getName().isEmpty()) {
             userRegistration.setName(registrationDTO.getName());
-        }else {
+        } else {
             throw new RuntimeException("Name Can't be null");
         }
-        if(registrationDTO.getEmail()!=null && !registrationDTO.getEmail().isEmpty()){
+        if (registrationDTO.getEmail() != null && !registrationDTO.getEmail().isEmpty()) {
             userRegistration.setEmail(registrationDTO.getEmail());
-        }else {
+        } else {
             throw new RuntimeException("Email Can't be null");
         }
-        if(registrationDTO.getAddress()!=null && !registrationDTO.getAddress().isEmpty()) {
+        if (registrationDTO.getAddress() != null && !registrationDTO.getAddress().isEmpty()) {
             userRegistration.setAddress(registrationDTO.getAddress());
-        }else {
+        } else {
             throw new RuntimeException("Address Can't be null");
         }
-        if(registrationDTO.getPassword()!=null && !registrationDTO.getPassword().isEmpty()) {
+        if (registrationDTO.getPassword() != null && !registrationDTO.getPassword().isEmpty()) {
             userRegistration.setPassword(registrationDTO.getPassword());
             registrationDTO.setPassword("*******");
-        }else {
+        } else {
             throw new RuntimeException("Password Can't be null");
         }
 
-        if(registrationDTO.getDob()!=null && !registrationDTO.getDob().isEmpty()) {
+        if (registrationDTO.getDob() != null && !registrationDTO.getDob().isEmpty()) {
             userRegistration.setDob(LocalDate.parse(registrationDTO.getDob()));
 
-        }else {
+        } else {
             throw new RuntimeException("DOB Can't be null");
         }
-        if(registrationDTO.getBloodGroup()!=null && !registrationDTO.getBloodGroup().isEmpty()) {
+        if (registrationDTO.getBloodGroup() != null && !registrationDTO.getBloodGroup().isEmpty()) {
             userRegistration.setBloodGroup(registrationDTO.getBloodGroup());
-        }else {
+        } else {
             throw new RuntimeException("Blood Group Can't be null");
         }
 
@@ -69,16 +74,90 @@ public class UserRegistrationService {
         userRegistration.setNewUser(true);
         userRegistration.setBlockedStatus(false);
 
-        repo.save(userRegistration);
+        userRepo.save(userRegistration);
         return registrationDTO;
     }
 
+    public boolean existsByUsernamer(String username) {
+        return userRepo.existsByUsername(username);
+    }
 
+    public String loginCheck(LoginDTO loginDTO, HttpSession session, Model model) {
+        session.setAttribute("loginAttempts", loginAttempts);
 
+        String username = loginDTO.getUsername();
+        String password = loginDTO.getPassword();
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+            model.addAttribute("errorMsg", "Login failed: Invalid Username ");
+            return "login";
+        }
+        if (!userRepo.existsByUsername(username)) {
+            model.addAttribute("errorMsg", "Login failed: Invalid Username ");
+            return "login";
+        }
+        if (userRepo.getBlockedStatusByUsername(username)) {
+            model.addAttribute("errorMsg", "User BLOCKED ");
+            return "login";
+        }
 
+        String pass = userRepo.getPasswordByUsername(username);
+        if (pass.equals(password)) {
+            session.setAttribute("data", userRepo.findByUsername(username).get());
+            return "redirect:/dashboard";
+        } else {
+            loginAttempts++;
+        }
 
+        if (loginAttempts > 3) {
+            userRepo.updateBlockedStatusByUsername(true, username);
+            session.invalidate(); // Invalidate the session to clear cache
+            model.addAttribute("errorMsg", "Login failed: User BLOCKED ");
 
+            return "login";
+        }
+        model.addAttribute("errorMsg", "Login failed: Invalid Password ");
+        model.addAttribute("times", loginAttempts);
+        return "login";
+    }
 
+    public String validateLogin(String currentPassword, String newPassword, String confirmPassword, Model mp, HttpSession session) {
+        UserRegistration userData = (UserRegistration) session.getAttribute("data");
+        if (!newPassword.equals(confirmPassword)) {
+            mp.addAttribute("errorMsg", "Password didn't Match.");
+            return "update-password";
+        }
+        if (currentPassword.equals(newPassword)) {
+            mp.addAttribute("errorMsg", "Can't set old password");
+            return "update-password";
+        }
+        if (!currentPassword.equals(userData.getPassword())) {
+            mp.addAttribute("errorMsg", "Wrong Password");
+            return "update-password";
+        }
+
+        String username = userData.getUsername();
+        System.out.println(userData.getUsername());
+        //---------- Updated the Password ---------
+        userRepo.updatePasswordByUsername(newPassword, username);
+        userRepo.updateNewUserByUsername(false, username);
+        mp.addAttribute("success", "Password updated successfully.");
+        return "login";
+    }
+
+    public String dashboard(Model mp, HttpSession session) {
+        UserRegistration userData = (UserRegistration) session.getAttribute("data");
+        mp.addAttribute("data", userData);
+        if (userData.isNewUser()) {
+            return "redirect:/update-password";
+        }
+        if (userData.getRole().equalsIgnoreCase("admin")) {
+
+            mp.addAttribute("allUsers", userRepo.findAllEndUsers());
+            return "admin-dashboard";
+        }
+        if (userData.getRole().equals("agent")) return "agent-dashboard";
+        return "enduser-dashboard";
+    }
 
 
 }
